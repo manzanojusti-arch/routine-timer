c75c7088c4fa47dbc72e41ca6ea79ddc0d5c6a0f
routinetimer <manzanojusti@gmail.com>
2026-09-21 13:08:03 -0300

    Refresh offline cache for workout tracking

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='routine-timer-v19';
-const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./Plantilla_Routine_Timer.xlsx'];
+const CACHE='routine-timer-v20';
+const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./Plantilla_Routine_Timer.xlsx','./silence.wav'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match('./index.html'))))});
