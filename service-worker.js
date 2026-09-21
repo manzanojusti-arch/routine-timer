c39c249a0549f97e8b414c1e197e95ecccffaf21
routinetimer <manzanojusti@gmail.com>
2026-09-21 13:21:06 -0300

    Refresh PWA cache for black theme

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='routine-timer-v22';
+const CACHE='routine-timer-v23';
 const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png','./Plantilla_Routine_Timer.xlsx'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
